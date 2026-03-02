41977fe25754b02a17263ccd5573e0f1e06f142b
lfireball <osatwje@gmail.com>
2026-03-02 20:52:15 +0200

    Update rewards.lua

--- wheel/rewards.lua
+++ wheel/rewards.lua
@@ -1,13 +1,10 @@
 return {
-  -- ВАЛЮТА
-  { name = "10 Coins", type = "item", item = "modid:coin", count = 10, weight = 40 },
-  { name = "50 Coins", type = "item", item = "modid:coin", count = 50, weight = 20 },
-  { name = "100 Coins", type = "item", item = "modid:coin", count = 100, weight = 10 },
+  { name = "10 Money", type = "item", item = "contenttweaker:money", count = 10, weight = 40 },
+  { name = "50 Money", type = "item", item = "contenttweaker:money", count = 50, weight = 20 },
+  { name = "100 Money", type = "item", item = "contenttweaker:money", count = 100, weight = 10 },
 
-  -- ПРЕДМЕТИ
   { name = "Diamond x1", type = "item", item = "minecraft:diamond", count = 1, weight = 10 },
   { name = "Diamond x3", type = "item", item = "minecraft:diamond", count = 3, weight = 5 },
 
-  -- ПУСТО
   { name = "Nothing", type = "none", weight = 15 }
 }
