ca2492e231e9878085a59eebb96e5d8f6d459012
lfireball <osatwje@gmail.com>
2026-03-02 20:33:41 +0200

    Create rewards.lua

---
+++ wheel/rewards.lua
@@ -0,0 +1,6 @@
+return {
+  { name = "50 Coins", type = "money", amount = 50, weight = 40 },
+  { name = "200 Coins", type = "money", amount = 200, weight = 15 },
+  { name = "Diamond x1", type = "item", item = "minecraft:diamond", count = 1, weight = 10 },
+  { name = "Nothing", type = "none", weight = 35 }
+}
